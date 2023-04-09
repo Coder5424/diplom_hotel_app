070f576399cf41cfeed1c60bdf575393bd2e273c
Coder5424 <crysis1228@mail.ru>
2023-04-09 18:22:50 +0300

    Add Booking, User and UserAdmin models

--- hotel_app/hotel/admin.py
+++ hotel_app/hotel/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, User, Booking
 
-admin.site.register(Room)
+
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+
+
+admin.site.register([Room, Booking])
+admin.site.register(User, UserAdmin)
--- hotel_app/hotel/models.py
+++ hotel_app/hotel/models.py
@@ -1,4 +1,20 @@
+import uuid
 from django.db import models
+
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254, unique=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+    password = models.CharField(max_length=20)
+    is_admin = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
 
 
 class Room(models.Model):
@@ -13,4 +29,17 @@ class Room(models.Model):
     capacity = models.IntegerField()
 
     def __str__(self):
-        return f'Room number {self.number}. {self.type} with {self.beds} beds for {self.capacity} people'
+        return f'room number {self.number}. {self.type} with {self.beds} beds for {self.capacity} people.'
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    check_in = models.DateField()
+    check_out = models.DateField()
+
+    def __str__(self):
+        return f'{self.user} has booked in {self.room} from {self.check_in} to {self.check_out}'
+
+
+
