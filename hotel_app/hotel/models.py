efcae231f3ab018eccbb9248be834ca30a186163
Coder5424 <crysis1228@mail.ru>
2023-04-09 21:28:57 +0300

    Add base urls, booking availability function and templates

--- hotel_app/hotel/models.py
+++ hotel_app/hotel/models.py
@@ -35,8 +35,8 @@ class Room(models.Model):
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    check_in = models.DateField()
-    check_out = models.DateField()
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
 
     def __str__(self):
         return f'{self.user} has booked in {self.room} from {self.check_in} to {self.check_out}'
---
+++ hotel_app/hotel/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import RoomList, BookingList
+
+urlpatterns = [
+    path('room_list/', RoomList.as_view(), name='RoomList'),
+    path('booking_list/', BookingList.as_view(), name='BookingList'),
+]
